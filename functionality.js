28efaf11ac5b0f8a75d1728e89869e20b3b714c8
Zihad-1883 <your@email.com>
2026-03-09 05:04:44 +0600

    button toggling with neccessary functionalities done

--- functionality.js
+++ functionality.js
@@ -3,22 +3,42 @@ const issuesCounter = document.getElementById('issues-counter');
 const buttonsContainer = document.getElementById('buttons-container');
 
 
+
 async function loadcards(){
+    // getting and toggling buttons 
+    const allBtn = document.createElement('button');
+    allBtn.textContent = "All"
+    allBtn.className = "btn btn-primary"
+    buttonsContainer.appendChild(allBtn)
+
+    const openBtn = document.createElement('button');
+    openBtn.textContent = "Open"
+    openBtn.className = "btn btn-outline"
+    buttonsContainer.appendChild(openBtn)
+
+
+    const closeBtn = document.createElement('button');
+    closeBtn.textContent = "Close"
+    closeBtn.className = "btn btn-outline"
+    buttonsContainer.appendChild(closeBtn)
+
+    const allButtons = [allBtn,openBtn,closeBtn];
+    allButtons.forEach(button => {
+        button.addEventListener('click',function(){
+            allButtons.forEach(btn => {
+                btn.classList.add('btn-outline');
+                btn.classList.remove('btn-primary')
+            })
+            button.classList.add('btn-primary');
+            button.classList.remove('btn-outline');
+        })
+    })    
+
+
+
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json();
     // console.log(data);
-
-    // getting buttons
-    const buttonDiv = document.createElement('div');
-    buttonDiv.innerHTML = `
-        <div class="bg-white shadow-xl p-6 mt-15 mb-4 w-3/4 mx-auto rounded-md space-x-4">
-          <button class=" btn btn-primary">All</button>
-          <button onclick="selectButton()" class="buttons btn btn-outline">Open</button>
-          <button onclick="selectButton()" class="buttons btn btn-outline">Closed</button>
-        </div>
-    `
-    buttonsContainer.appendChild(buttonDiv)
-
 
     // getting cards
     data.data.forEach(card => {
@@ -43,12 +63,10 @@ async function loadcards(){
         `
         // console.log(card.title)
         cardsContainer.appendChild(newCard)
-
-
-
-
         // issues counter
         issuesCounter.innerText = cardsContainer.children.length + " Issues"
+
+
 
          // priority logic
         function priorityLogic(){
@@ -73,23 +91,113 @@ async function loadcards(){
                 return "./assets/Closed-Status.png"
             }
         }
+
+    
+        // open button
+        openBtn.addEventListener('click',() => {
+            cardsContainer.innerHTML = ""
+            data.data.forEach(card => {
+            if(card.status === "open"){
+                        const newCard = document.createElement('div');
+            newCard.innerHTML = `
+                <div class="bg-white shadow-xl p-4 border-t-5 rounded-md ${card.status === "open" ? "border-t-[#00A96E]" : "border-t-purple-600"}">
+                    <div class="flex justify-between items-center mb-3">
+                        <img src="${statusIcon()}" alt="${statusIcon()}">
+                        <span id = "priority-span" class="text-[#EF4444] bg-[#FEECEC] px-5 py-1 rounded-xl ${priorityLogic()} ">${card.priority}</span>
+                    </div>
+                    <h2 class="font-semibold mb-2">${card.title}</h2>
+                    <p class="text-[12px] text-[#64748B] mb-3 line-clamp-2">${card.description}</p>
+                    <div class="flex flex-wrap gap-2 mb-4">
+                        <button class="flex justify-center items-center gap-[2px] text-[#EF4444] bg-[#FEECEC] px-5 py-1 border rounded-2xl"><img src="./assets/BugDroid.png" alt="">${card.labels[0]}</button>
+                        <button class="flex justify-center items-center gap-[2px] text-[#D97706] bg-[#fcf5d9] px-5 py-1 border rounded-xl"><img src="./assets/BugDroid.png" alt="">${card.labels[1]}</button>
+                    </div>
+                    <hr>
+                    <p class="mt-4 mb-2 text-[#64748B]">#1 by ${card.author}</p>
+                    <p class="mb-4 text-[#64748B]">${card.createdAt}</p>
+                </div>
+            `
+            // console.log(card.title)
+            cardsContainer.appendChild(newCard)
+            
+        
+            }
+        })
+        // issues counter
+        issuesCounter.innerText = cardsContainer.children.length + " Issues"
+
+
+        })
    
+
+        // close button
+        closeBtn.addEventListener('click',() => {
+            cardsContainer.innerHTML = ""
+            data.data.forEach(card => {
+                if(card.status === "closed"){
+                            const newCard = document.createElement('div');
+                newCard.innerHTML = `
+                    <div class="bg-white shadow-xl p-4 border-t-5 rounded-md ${card.status === "open" ? "border-t-[#00A96E]" : "border-t-purple-600"}">
+                        <div class="flex justify-between items-center mb-3">
+                            <img src="${statusIcon()}" alt="${statusIcon()}">
+                            <span id = "priority-span" class="text-[#EF4444] bg-[#FEECEC] px-5 py-1 rounded-xl ${priorityLogic()} ">${card.priority}</span>
+                        </div>
+                        <h2 class="font-semibold mb-2">${card.title}</h2>
+                        <p class="text-[12px] text-[#64748B] mb-3 line-clamp-2">${card.description}</p>
+                        <div class="flex flex-wrap gap-2 mb-4">
+                            <button class="flex justify-center items-center gap-[2px] text-[#EF4444] bg-[#FEECEC] px-5 py-1 border rounded-2xl"><img src="./assets/BugDroid.png" alt="">${card.labels[0]}</button>
+                            <button class="flex justify-center items-center gap-[2px] text-[#D97706] bg-[#fcf5d9] px-5 py-1 border rounded-xl"><img src="./assets/BugDroid.png" alt="">${card.labels[1]}</button>
+                        </div>
+                        <hr>
+                        <p class="mt-4 mb-2 text-[#64748B]">#1 by ${card.author}</p>
+                        <p class="mb-4 text-[#64748B]">${card.createdAt}</p>
+                    </div>
+                `
+                // console.log(card.title)
+                cardsContainer.appendChild(newCard)
+                
+                }
+            })
+            
+        // issues counter
+        issuesCounter.innerText = cardsContainer.children.length + " Issues"
+
+        })
+
+
+        // all button
+        allBtn.addEventListener('click',() => {
+            cardsContainer.innerHTML = ""
+            data.data.forEach(card => {
+                const newCard = document.createElement('div');
+                newCard.innerHTML = `
+                <div class="bg-white shadow-xl p-4 border-t-5 rounded-md ${card.status === "open" ? "border-t-[#00A96E]" : "border-t-purple-600"}">
+                    <div class="flex justify-between items-center mb-3">
+                        <img src="${statusIcon()}" alt="${statusIcon()}">
+                        <span id = "priority-span" class="text-[#EF4444] bg-[#FEECEC] px-5 py-1 rounded-xl ${priorityLogic()} ">${card.priority}</span>
+                    </div>
+                    <h2 class="font-semibold mb-2">${card.title}</h2>
+                    <p class="text-[12px] text-[#64748B] mb-3 line-clamp-2">${card.description}</p>
+                    <div class="flex flex-wrap gap-2 mb-4">
+                        <button class="flex justify-center items-center gap-[2px] text-[#EF4444] bg-[#FEECEC] px-5 py-1 border rounded-2xl"><img src="./assets/BugDroid.png" alt="">${card.labels[0]}</button>
+                        <button class="flex justify-center items-center gap-[2px] text-[#D97706] bg-[#fcf5d9] px-5 py-1 border rounded-xl"><img src="./assets/BugDroid.png" alt="">${card.labels[1]}</button>
+                    </div>
+                    <hr>
+                    <p class="mt-4 mb-2 text-[#64748B]">#1 by ${card.author}</p>
+                    <p class="mb-4 text-[#64748B]">${card.createdAt}</p>
+                </div>
+            `
+            // console.log(card.title)
+            cardsContainer.appendChild(newCard)
+        })
+        // issues counter
+        issuesCounter.innerText = cardsContainer.children.length + " Issues"
+        })
+
+
     });
 
 }
 
 loadcards();
-
-async function selectButton(){
-    const buttons = document.querySelectorAll('#buttons-container button');
-    buttons.forEach(button => {
-        button.classList.add('btn-outline');
-        button.classList.remove('btn-primary')
-    })
-
-    buttons.classList.add('btn-primary');
-    buttons.classList.remove('btn-outline');
-}
-
 
 
